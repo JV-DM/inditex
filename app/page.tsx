25894c938847a7417ff50a8138015bf164939639
JV-DM <javidmwork@outlook.com>
2025-07-15 14:00:11 +0200

    add logs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { PhonesList } from '../components/PhonesList'
 
-export default function Home() {
+const Home = () => {
+  console.log('All env vars:', Object.keys(process.env))
+  console.log('Your var:', process.env.NEXT_PUBLIC_API_KEY)
+
   return <PhonesList />
 }
+
+export default Home
